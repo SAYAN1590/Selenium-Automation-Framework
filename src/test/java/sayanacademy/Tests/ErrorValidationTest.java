e7f255102af0f03df99cd97223625c8c59bf11d5
SAYAN1590 <sayanpaul671@gmail.com>
2025-02-22 13:47:55 +0530

    Error Test Modified

--- src/test/java/sayanacademy/Tests/ErrorValidationTest.java
+++ src/test/java/sayanacademy/Tests/ErrorValidationTest.java
@@ -1,8 +1,11 @@
 package sayanacademy.Tests;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import sayanacademy.TestComponents.BaseTest;
@@ -10,11 +13,21 @@ import sayanacademy.TestComponents.RetryFailedTest;
 
 public class ErrorValidationTest extends BaseTest {
     
-    @Test(groups={"ErrorHandling"}, retryAnalyzer= RetryFailedTest.class)
-    public void errorValidation() throws IOException, InterruptedException {
+    @Test(groups={"ErrorHandling"}, retryAnalyzer= RetryFailedTest.class, dataProvider = "getErrorData")
+    public void errorValidation(HashMap<String, String> input) throws IOException, InterruptedException {
 
-        landingPage.loginApp("chhutkuritam@gmail.com","Iamsrk@1591");
+        landingPage.loginApp(input.get("email"),input.get("password"));
         Assert.assertEquals("Incorrect email or password.", landingPage.getErrorMessage());   
+    }
+    
+    @DataProvider
+	public Object[][] getErrorData() throws IOException {
+	    	
+	    	
+	    	List <HashMap<String, String>>data = getJsonDataToMap(System.getProperty("user.dir")+"\\src\\test\\java\\sayanacademy\\data\\ErrorLoginData.json");
+	    	 
+	    	return new Object[][] {{data.get(0)},{data.get(1)}};
+    	
     }
 
 }
